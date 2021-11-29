e3e7d6fb5997e993fd6d66330cdd569dde8ec06c
ycs-201807005 <leen3357@gmail.com>
2021-11-29 23:21:43 +0900

    [-]

--- book_management_program/book_management_program/Manager/MemberManager.cs
+++ book_management_program/book_management_program/Manager/MemberManager.cs
@@ -10,13 +10,14 @@ namespace book_management_program.Manager
     class MemberManager
     {
 
-        /* 회원 정보 등록
+        /* 회원 정보 등록 */
         public static bool MemInfoInsert(Model.Member member)
         {
-            string sql = "INSERT INTO member VALUES(입력)";
-            bool result = MySql_Util.Instance.Insert_Sql(sql);
-            return result;
-        } */
+            //string sql = "INSERT INTO member VALUES(입력)";
+            //bool result = MySql_Util.Instance.Insert_Sql(sql);
+            //return result;
+            return true;
+        }
 
 
         //string sql = "SELECT * FROM conntest;"
